52aca8d0ff176a82b461dcef56fc7673d831de26
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-20 15:16:36 +0700

    Tours Page - Setup

--- app/(dashboard)/tours/page.js
+++ app/(dashboard)/tours/page.js
@@ -1,7 +1,25 @@
+import ToursPage from "@/components/ToursPage";
+import { getAllTours } from "@/utils/action";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import React from "react";
 
-const ToursPage = () => {
-  return <div>ToursPage</div>;
+const AllToursPage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["tours"],
+    queryFn: () => getAllTours(),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ToursPage />
+    </HydrationBoundary>
+  );
 };
 
-export default ToursPage;
+export default AllToursPage;
