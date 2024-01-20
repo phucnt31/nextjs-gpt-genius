58e523f6ac09d769fee0ca65cf074de88772b7cb
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-20 15:30:32 +0700

    Tours List

--- components/ToursPage.jsx
+++ components/ToursPage.jsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { getAllTours } from "@/utils/action";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import TourList from "./TourList";
 
 const ToursPage = () => {
-  return <div>ToursPage</div>;
+  const { data, isPending } = useQuery({
+    queryKey: ["tours"],
+    queryFn: () => getAllTours(),
+  });
+
+  return (
+    <>
+      {isPending ? <span className="loading"></span> : <TourList data={data} />}
+    </>
+  );
 };
 
 export default ToursPage;
