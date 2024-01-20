d71bb15a53fba97d39aab4781215ca10ee8f3225
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-20 10:28:51 +0700

    SidebarHeader

---
+++ components/ThemeToggle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ThemeToggle = () => {
+  return (
+    <button type="button" className="btn btn-primary btn-sm">
+      ThemeToggle
+    </button>
+  );
+};
+
+export default ThemeToggle;
