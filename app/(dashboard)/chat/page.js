2ac47dc87cbea05bcc249dc0a6b0717ccb6cfb65
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-20 11:13:41 +0700

    Chat structure

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,7 +1,12 @@
+import Chat from "@/components/Chat";
 import React from "react";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <>
+      <Chat />
+    </>
+  );
 };
 
 export default ChatPage;
---
+++ components/Chat.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Chat = () => {
+  const [text, setText] = useState("");
+  const [message, setMessage] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+  };
+
+  return (
+    <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
+      <div>
+        <h2 className="text-5xl">messages</h2>
+      </div>
+      <form onSubmit={handleSubmit} className="max-w-4xl pt-12">
+        <div className="join w-full">
+          <input
+            type="text"
+            placeholder="Message GeniusGPT"
+            className="input input-bordered join-item w-full"
+            value={text}
+            required
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button type="submit" className="btn btn-primary join-item">
+            ask question
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Chat;
