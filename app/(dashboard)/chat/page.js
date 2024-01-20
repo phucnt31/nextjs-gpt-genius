d60071eb53953c8646ffcae8dbae4e6d1d5783bb
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-20 11:51:31 +0700

    Setup react query

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,11 +1,18 @@
 import Chat from "@/components/Chat";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import React from "react";
 
 const ChatPage = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <Chat />
-    </>
+    </HydrationBoundary>
   );
 };
 
