79533b50777ee28d8b691ae2cfa9c23017c51dc1
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-20 15:52:42 +0700

    Generate image

--- utils/action.js
+++ utils/action.js
@@ -117,3 +117,17 @@ export const getSingleTour = async (id) => {
     },
   });
 };
+
+export const generateTourImage = async ({ city, country }) => {
+  try {
+    const tourImage = await openai.images.generate({
+      prompt: `a panoramic view of the ${city} ${country}`,
+      n: 1,
+      size: "512x512",
+    });
+    return tourImage?.data[0]?.url;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
