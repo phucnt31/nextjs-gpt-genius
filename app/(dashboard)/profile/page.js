91bbed9b042eb04e3eeb5099df66752d2fd09e55
phucnt31 <nguyenthienphuc.02@gmail.com>
2024-01-20 16:33:52 +0700

    Generate and Show Tokens

--- app/(dashboard)/profile/page.js
+++ app/(dashboard)/profile/page.js
@@ -1,8 +1,20 @@
-import { UserProfile } from "@clerk/nextjs";
+import { fetchUserTokensById } from "@/utils/action";
+import { UserProfile, auth } from "@clerk/nextjs";
 import React from "react";
 
-const ProfilePage = () => {
-  return <UserProfile />;
+const ProfilePage = async () => {
+  const { userId } = auth();
+
+  const currentTokens = await fetchUserTokensById(userId);
+
+  return (
+    <div>
+      <h2 className="mb-8 ml-8 text-xl font-extrabold">
+        Token Amount : {currentTokens}
+      </h2>
+      <UserProfile />
+    </div>
+  );
 };
 
 export default ProfilePage;
--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -1,9 +1,12 @@
+import { fetchOrGenerateTokens } from "@/utils/action";
 import { UserButton, auth, currentUser } from "@clerk/nextjs";
 import React from "react";
 
 const MemberProfile = async () => {
   const user = await currentUser();
-  // const { userId } = auth();
+  const { userId } = auth();
+
+  await fetchOrGenerateTokens(userId);
 
   return (
     <div className="px-4 flex items-center gap-2">
